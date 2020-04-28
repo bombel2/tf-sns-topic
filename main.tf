3867269be9757a7a31d099910b6833a8b9e53450
Rolf Streefkerk <r.streefkerk@gmail.com>
2020-04-28 14:23:55 +0700

    added integration SQS & Lambda

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ locals {
   sqs_topic_name    = "${local.resource_name_prefix}-${var.topic_name}-sqs"
 }
 
+# -----------------------------------------------------------------------------
+# Variables: Lambda integration
+# -----------------------------------------------------------------------------
 resource "aws_sns_topic" "lambda" {
   count = var.lambda_sns_topic ? 1 : 0
 
@@ -20,6 +23,27 @@ resource "aws_sns_topic" "lambda" {
   }
 }
 
+resource "aws_sns_topic_subscription" "lambda" {
+  count = var.lambda_sns_topic ? 1 : 0
+
+  topic_arn = aws_sns_topic.lambda.arn
+  protocol  = "lambda"
+  endpoint  = var.lambda_function_arn
+}
+
+resource "aws_lambda_permission" "lambda" {
+  count = var.lambda_sns_topic ? 1 : 0
+  
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arn
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.lambda.arn
+}
+
+# -----------------------------------------------------------------------------
+# Variables: SQS integration
+# -----------------------------------------------------------------------------
 resource "aws_sns_topic" "sqs" {
   count = var.sqs_sns_topic ? 1 : 0
 
@@ -33,4 +57,39 @@ resource "aws_sns_topic" "sqs" {
     Environment = var.namespace
     Name        = var.resource_tag_name
   }
+}
+
+resource "aws_sns_topic_subscription" "sqs" {
+  count = var.sqs_sns_topic ? 1 : 0
+
+  topic_arn = aws_sns_topic.sqs.arn
+  protocol  = "sqs"
+  endpoint  = var.sqs_arn
+}
+
+resource "aws_sqs_queue_policy" "_" {
+  count = var.sqs_sns_topic ? 1 : 0
+
+  queue_url = var.sqs_id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${var.sqs_arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.sqs.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
--- variables.tf
+++ variables.tf
@@ -17,25 +17,25 @@ variable "resource_tag_name" {
 # -----------------------------------------------------------------------------
 # Variables: SNS
 # -----------------------------------------------------------------------------
+variable "topic_name" {
+  description = "Name of the SNS topic"
+}
+
+# -----------------------------------------------------------------------------
+# Variables: Lambda integration
+# -----------------------------------------------------------------------------
 variable "lambda_sns_topic" {
   description = "Lambda integration point for this SNS topic"
   type        = bool
   default     = true
 }
 
-variable "sqs_sns_topic" {
-  description = "Lambda integration point for this SNS topic"
-  type        = bool
-  default     = false
+variable "lambda_function_arn" {
+  description = "Lambda function ARN"
+  type        = string
+  default     = null
 }
 
-variable "topic_name" {
-  description = "Name of the SNS topic"
-}
-
-# -----------------------------------------------------------------------------
-# Variables: CloudWatch logging optional
-# -----------------------------------------------------------------------------
 variable "lambda_success_feedback_role_arn" {
   description = "IAM Role for success feedback CloudWatch logs"
   type        = string
@@ -51,6 +51,27 @@ variable "lambda_failure_feedback_role_arn" {
 variable "lambda_success_feedback_sample_rate" {
   description = "Success feedback sample rate between 0 and 1"
   type        = number
+  default     = null
+}
+
+# -----------------------------------------------------------------------------
+# Variables: SQS Integration
+# -----------------------------------------------------------------------------
+variable "sqs_sns_topic" {
+  description = "Lambda integration point for this SNS topic"
+  type        = bool
+  default     = false
+}
+
+variable "sqs_id" {
+  description = "SQS ID"
+  type        = string
+  default     = null
+}
+
+variable "sqs_arn" {
+  description = "SQS ARN"
+  type        = string
   default     = null
 }
 
